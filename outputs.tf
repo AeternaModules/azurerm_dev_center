8dd2ad0f73fd15f512e922c716cbc985d6a52010
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:14 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "dev_centers" {
-  description = "All dev_center resources"
-  value       = azurerm_dev_center.dev_centers
-}
 output "dev_centers_dev_center_uri" {
-  description = "List of dev_center_uri values across all dev_centers"
-  value       = [for k, v in azurerm_dev_center.dev_centers : v.dev_center_uri]
+  description = "Map of dev_center_uri values across all dev_centers, keyed the same as var.dev_centers"
+  value       = { for k, v in azurerm_dev_center.dev_centers : k => v.dev_center_uri }
 }
 output "dev_centers_identity" {
-  description = "List of identity values across all dev_centers"
-  value       = [for k, v in azurerm_dev_center.dev_centers : v.identity]
+  description = "Map of identity values across all dev_centers, keyed the same as var.dev_centers"
+  value       = { for k, v in azurerm_dev_center.dev_centers : k => v.identity }
 }
 output "dev_centers_location" {
-  description = "List of location values across all dev_centers"
-  value       = [for k, v in azurerm_dev_center.dev_centers : v.location]
+  description = "Map of location values across all dev_centers, keyed the same as var.dev_centers"
+  value       = { for k, v in azurerm_dev_center.dev_centers : k => v.location }
 }
 output "dev_centers_name" {
-  description = "List of name values across all dev_centers"
-  value       = [for k, v in azurerm_dev_center.dev_centers : v.name]
+  description = "Map of name values across all dev_centers, keyed the same as var.dev_centers"
+  value       = { for k, v in azurerm_dev_center.dev_centers : k => v.name }
 }
 output "dev_centers_project_catalog_item_sync_enabled" {
-  description = "List of project_catalog_item_sync_enabled values across all dev_centers"
-  value       = [for k, v in azurerm_dev_center.dev_centers : v.project_catalog_item_sync_enabled]
+  description = "Map of project_catalog_item_sync_enabled values across all dev_centers, keyed the same as var.dev_centers"
+  value       = { for k, v in azurerm_dev_center.dev_centers : k => v.project_catalog_item_sync_enabled }
 }
 output "dev_centers_resource_group_name" {
-  description = "List of resource_group_name values across all dev_centers"
-  value       = [for k, v in azurerm_dev_center.dev_centers : v.resource_group_name]
+  description = "Map of resource_group_name values across all dev_centers, keyed the same as var.dev_centers"
+  value       = { for k, v in azurerm_dev_center.dev_centers : k => v.resource_group_name }
 }
 output "dev_centers_tags" {
-  description = "List of tags values across all dev_centers"
-  value       = [for k, v in azurerm_dev_center.dev_centers : v.tags]
+  description = "Map of tags values across all dev_centers, keyed the same as var.dev_centers"
+  value       = { for k, v in azurerm_dev_center.dev_centers : k => v.tags }
 }
 
